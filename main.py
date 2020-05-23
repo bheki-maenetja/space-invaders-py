bb26df4202d43c836c2ec34a8f402382e5b831c2
bheki-maenetja <bhekimaenetja@gmail.com>
2020-05-23 11:36:23 +0200

    added player sprite

--- main.py
+++ main.py
@@ -1,66 +1,62 @@
 # pylint: disable=no-member
 # pylint: disable=too-many-function-args
+# pylint: disable=no-name-in-module
 # Skeleton for a new project
 import pygame
+from pygame.locals import (
+  K_RIGHT,
+  K_LEFT
+)
+
 import random
 from os import path
+import colours
 
-# MAIN FUNCTION
-def main(): 
-  # INITIALISE PYGAME AND CREATE WINDOW
-  WIDTH = 360
-  HEIGHT = 480
-  FPS = 30
+# INITIALISE PYGAME AND CREATE WINDOW
+WIDTH = 540
+HEIGHT = 720
+FPS = 60
 
-  pygame.init()
-  pygame.mixer.init()
-  screen = pygame.display.set_mode((WIDTH, HEIGHT))
-  pygame.display.set_caption("My Game")
-  clock = pygame.time.Clock()
-
-  # GAME LOOP
-  running = True
-
-  while running:
-    # Process Input (events)
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT: # check for closing the window
-        running = False
-
-    # Update
-    all_sprites.update()
-    
-    # Draw / Render
-    screen.fill(MAGENTA)
-    all_sprites.draw(screen)
-
-    # AFTER Drawing Everything, Flip the Display
-    pygame.display.flip()
-
-    # Keep Game Loop Running at Given FPS
-    clock.tick(FPS)
-
-# USEFUL COLOURS
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
-GREEN = (0, 255, 0)
-ORANGE = (255, 165, 0)
-MAGENTA = (255, 0, 255)
-OLIVE = (128, 128, 0)
-KHAKI = (240, 230, 140)
-TEAL = (0, 128, 128)
-SIENNA = (160, 83, 45)
-TAN = (210, 180, 140)
-INDIGO = (75, 0, 130)
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My Game")
+clock = pygame.time.Clock()
 
 # LOAD ALL SPRITE IMAGES
 img_dir = path.join(path.dirname(__file__), 'img')
 
 # SETUP SPRITES
+
+# Sprite Classes
+class Player(pygame.sprite.Sprite):
+  def __init__(self):
+    super(Player, self).__init__()
+    self.image = pygame.Surface((50, 50))
+    self.image.fill(colours.RED)
+    self.rect = self.image.get_rect(center=(WIDTH / 2, HEIGHT - 55))
+    self.speed = 5
+  
+  def update(self):
+    key_state = pygame.key.get_pressed()
+    if key_state[K_LEFT]:
+      self.rect.move_ip(-self.speed, 0)
+    if key_state[K_RIGHT]:
+      self.rect.move_ip(self.speed, 0)
+
+    self.boundary_check()
+    
+  def boundary_check(self):
+    if self.rect.x < 0:
+      self.rect.x = 0
+    if self.rect.right > WIDTH:
+      self.rect.right = WIDTH
+
+# Sprites and Groups
 all_sprites = pygame.sprite.Group()
+player = Player()
+
+all_sprites.add(player)
 
 # GAME VARIABLES
 
@@ -68,4 +64,24 @@ all_sprites = pygame.sprite.Group()
 
 # UTILITY FUNCTIONS
 
-if __name__ == '__main__': main()
+# GAME LOOP
+running = True
+
+while running:
+  # Process Input (events)
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT: # check for closing the window
+      running = False
+
+  # Update
+  all_sprites.update()
+  
+  # Draw / Render
+  screen.fill(colours.WHITE)
+  all_sprites.draw(screen)
+
+  # AFTER Drawing Everything, Flip the Display
+  pygame.display.flip()
+
+  # Keep Game Loop Running at Given FPS
+  clock.tick(FPS)
