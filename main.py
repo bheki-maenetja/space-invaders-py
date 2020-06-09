92280eb80a2a4cc01b22ae1afa11b62b0585a267
bheki-maenetja <bhekimaenetja@gmail.com>
2020-06-09 11:19:28 +0200

    fixed mothership distribution issue

--- main.py
+++ main.py
@@ -74,9 +74,9 @@ class Bullet(pygame.sprite.Sprite):
       self.kill()
 
 class Alien(pygame.sprite.Sprite):
-  def __init__(self, x, y, speed):
+  def __init__(self, x, y, width, height,speed):
     super(Alien, self).__init__()
-    self.image = pygame.Surface((20, 20))
+    self.image = pygame.Surface((width, height))
     self.image.fill(choice(colours.ALL_COLOURS)[1])
     self.rect = self.image.get_rect(center=(x,y))
     self.speed = speed
@@ -98,7 +98,7 @@ class Alien(pygame.sprite.Sprite):
 
 class MotherShip(Alien):
   def __init__(self, x, y):
-    super(MotherShip, self).__init__(x,y,0)
+    super(MotherShip, self).__init__(x,y,50,50,0)
     self.image = pygame.Surface((50, 50))
     self.image.fill(colours.BLACK)
 
@@ -195,15 +195,14 @@ def draw_menu(screen):
 def set_aliens():
   for i in range(60, 301, 60):
     for j in range(20, WIDTH, 40):
-      new_alien = Alien(j, i, alien_speed)
+      new_alien = Alien(j, i, 20, 20, alien_speed)
       all_sprites.add(new_alien)
       aliens.add(new_alien)
 
 def set_motherships(num_ships):
   for i in range(num_ships):
     print((i+1) * (WIDTH // (num_ships+ 1)))
-    new_mothership = MotherShip((i+1) * (WIDTH // (num_ships+ 1)), 50)
-    print(new_mothership.rect.width)
+    new_mothership = MotherShip((i+1) * (WIDTH // (num_ships+ 1)), 60)
     all_sprites.add(new_mothership)
     
 def set_game_settings(level):
@@ -217,7 +216,7 @@ def set_game_settings(level):
 set_game_settings(game_level)
 # set_aliens()
 # num_waves -= 1
-set_motherships(5)
+set_motherships(7)
 
 # GAME LOOP
 running = True
