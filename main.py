809e1d9223a2487e14c03529f2c45d8865b44017
bheki-maenetja <bhekimaenetja@gmail.com>
2020-06-14 11:11:50 +0200

    put game settings in different file

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from pygame.locals import (
 from random import choice
 from os import path
 import colours
+import game_settings
 
 # INITIALISE PYGAME AND CREATE WINDOW
 WIDTH = 540
@@ -150,48 +151,7 @@ mothership_lives = 0
 alien_hit_points = 0
 
 # Game Settings
-game_settings = { # Aliens speeds: 1,2,4,5,8,10 & 20 (maybe)
-  1: {
-    'alien_speed': 1,
-    'alien_fire_rate': 2.5,
-    'num_waves': 4,
-    'num_ships': 1,
-    'mothership_lives': 3,
-    'alien_hit_points': 10
-  },
-  2: {
-    'alien_speed': 2,
-    'alien_fire_rate': 2,
-    'num_waves': 6,
-    'num_ships': 2,
-    'mothership_lives': 4,
-    'alien_hit_points': 20
-  },
-  3: {
-    'alien_speed': 4,
-    'alien_fire_rate': 1.5,
-    'num_waves': 8,
-    'num_ships': 3,
-    'mothership_lives': 5,
-    'alien_hit_points': 50
-  },
-  4: {
-    'alien_speed': 5,
-    'alien_fire_rate': 1,
-    'num_waves': 10,
-    'num_ships': 4,
-    'mothership_lives': 6,
-    'alien_hit_points': 100
-  },
-  5: {
-    'alien_speed': 8,
-    'alien_fire_rate': 0.5,
-    'num_waves': 12,
-    'num_ships': 5,
-    'mothership_lives': 7,
-    'alien_hit_points': 150
-  },
-}
+game_settings = game_settings.main_settings
 
 # TEXT
 font_name = pygame.font.match_font('arial')
