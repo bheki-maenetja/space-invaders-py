3efecaf0e49e5582da2bb5fdfef55e22468528aa
bheki-maenetja <bhekimaenetja@gmail.com>
2020-09-10 10:33:36 +0100

    modified game settings

--- game_settings.py
+++ game_settings.py
@@ -2,7 +2,7 @@ main_settings = { # Aliens speeds: 1,2,4,5,8,10 & 20 (maybe)
   1: {
     'alien_speed': 1,
     'alien_fire_rate': 2.5,
-    'num_waves': 4,
+    'num_waves': 2,
     'num_ships': 1,
     'mothership_lives': 3,
     'alien_hit_points': 10
@@ -10,7 +10,7 @@ main_settings = { # Aliens speeds: 1,2,4,5,8,10 & 20 (maybe)
   2: {
     'alien_speed': 2,
     'alien_fire_rate': 2,
-    'num_waves': 6,
+    'num_waves': 3,
     'num_ships': 2,
     'mothership_lives': 4,
     'alien_hit_points': 20
@@ -18,7 +18,7 @@ main_settings = { # Aliens speeds: 1,2,4,5,8,10 & 20 (maybe)
   3: {
     'alien_speed': 4,
     'alien_fire_rate': 1.5,
-    'num_waves': 8,
+    'num_waves': 5,
     'num_ships': 3,
     'mothership_lives': 5,
     'alien_hit_points': 50
@@ -26,7 +26,7 @@ main_settings = { # Aliens speeds: 1,2,4,5,8,10 & 20 (maybe)
   4: {
     'alien_speed': 5,
     'alien_fire_rate': 1,
-    'num_waves': 10,
+    'num_waves': 6,
     'num_ships': 4,
     'mothership_lives': 6,
     'alien_hit_points': 100
@@ -34,7 +34,7 @@ main_settings = { # Aliens speeds: 1,2,4,5,8,10 & 20 (maybe)
   5: {
     'alien_speed': 8,
     'alien_fire_rate': 0.5,
-    'num_waves': 12,
+    'num_waves': 7,
     'num_ships': 5,
     'mothership_lives': 7,
     'alien_hit_points': 150
